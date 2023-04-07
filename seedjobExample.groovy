cd37cd19063dfd9c8088323df8918c0ef0cbaab5
rahulgandhi28 <72257523+rahulgandhi28@users.noreply.github.com>
2023-04-07 13:56:36 +0530

    Update seedjobExample.groovy

--- seedjobExample.groovy
+++ seedjobExample.groovy
@@ -3,17 +3,17 @@ pipeline {
     stages {
         stage('Build') { 
             steps {
-                // 
+                echo 'Hello'
             }
         }
         stage('Test') { 
             steps {
-                // 
+                echo 'rahul'
             }
         }
         stage('Deploy') { 
             steps {
-                // 
+                echo 'gandhi'
             }
         }
     }
